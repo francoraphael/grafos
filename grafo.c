9421c0d23f73ab7887981bbac022d765682b3152
g0r <g0r@localhost.localdomain>
2019-03-28 00:04:09 -0300

    add gitignore and fix previus grafos file

--- grafo.c
+++ grafo.c
@@ -2,69 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*
+Programa implementa leitura  de um arquivo contendo 
+as informações sobre vértices e arestas de um grafo 
+e armazena em um vetor de listas.
+
+Dificuldades
+	- Relembrar as nuances de ponteiros em C
+	- Ferramenta de debugger
+	- Tempo
+*/
+
 typedef struct estrutura{
 	int info;
 	struct estrutura *prox;
 }NO;
-
-typedef struct{
-	NO *inicio;
-}GRAFO;
-
-void inicializar(GRAFO *grafo){
-	grafo->inicio = NULL;
-}
-
-void mostrar(GRAFO li){
-	NO *p = li.inicio;
-	printf("\nInicio->[");
-	while(p){
-		printf("%d", p->info);
-		p = p->prox;
-		if(p)
-			printf(", ");
-	}
-	printf("]\n");
-}
-
-NO* buscaSeq(int info, GRAFO li, NO **ant){
-	NO *p = li.inicio;
-	*ant = NULL;
-	while(p){
-		if(p->info == info)
-			return p;
-		*ant = p;
-		p = p->prox;
-	}
-	return NULL;
-}
-
-NO* ultimoElemento(GRAFO li){
-	NO *p = li.inicio;
-	if(p)
-		while(p->prox)
-			p = p->prox;
-	return p;
-}
-
-void inserirNoFim(int info, GRAFO *li){
-	NO *novoNo = (NO*)malloc(sizeof(NO));
-	NO *ant = ultimoElemento(*li);
-	novoNo->info = info;
-	novoNo->prox = NULL;
-	if(!ant)
-		li->inicio = novoNo;
-	else{
-		ant->prox = novoNo;
-	}
-}
-
-void inserirNoInicio(int info, GRAFO *li){
-	NO *novoNo = (NO*)malloc(sizeof(NO));
-	novoNo->info = info;
-	novoNo->prox = li->inicio;
-	li->inicio = novoNo;
-}
 
 int input(char str[], int size) {
 	int i = 0;
@@ -81,27 +33,40 @@ int input(char str[], int size) {
 	return i;
 }
 
-void alocaGrafo(GRAFO *grafo, int qtdVertices){
-	int i = 0;
-	grafo = (GRAFO *) malloc(qtdVertices * sizeof(GRAFO));
-
-	for(i = 0; i<qtdVertices; i++)
-		inicializar(&grafo[i]);
-}
-
-void tokeniza(int *vet, char *token){
+void tokeniza(int *vector, char *token){
 	int i = 0;
 	while(token != NULL){
-		vet[i++] = atoi(token);
+		vector[i++] = atoi(token);
 		token = strtok(NULL, " ");
+	}
+}
+
+void insere(NO** grafo, int info, int vertex){
+	if(grafo[vertex] == NULL){
+		grafo[vertex] = (NO*) malloc(sizeof(NO*));
+		grafo[vertex]->info = info;
+		grafo[vertex]->prox = NULL;
+	}else{
+		NO* aux = (NO*) malloc(sizeof(NO*));
+		aux->prox = grafo[vertex]->prox;
+		aux->info = info;
+		grafo[vertex]->prox = aux;
+	}
+}
+
+void mostra(NO** grafo, int vertex){
+	NO *aux = grafo[vertex];
+	while(aux != NULL){
+		printf("%d\n", aux->info);
+		aux = aux->prox;
 	}
 }
 
 int main(){
 	char nomeArquivo[25], buffer[10], *token;
-	int vet[2], i, contador;
-	GRAFO *grafo;
+	int i, vector[2], nroVertices;
 	FILE *arq;
+	NO **grafo;
 
 	printf("Digite o nome do arquivo: ");
 	input(nomeArquivo, 25);
@@ -112,18 +77,14 @@ int main(){
 
 	fgets(buffer, sizeof(buffer), arq);
 	token = strtok(buffer, " ");
-	tokeniza(vet, token);
-	alocaGrafo(grafo, vet[0]);
-	contador = vet[1];
+	tokeniza(vector, token);
+	nroVertices = vector[0];
+	grafo = (NO**) malloc(nroVertices * sizeof(NO*));
 
-	for(i = 0; i<contador; i++){
+	for(i=0; i<nroVertices; i++){
 		fgets(buffer, sizeof(buffer), arq);
 		token = strtok(buffer, " ");
-		tokeniza(vet, token);
-		inserirNoInicio(vet[1], &grafo[vet[0]]);
+		tokeniza(vector, token);
+		insere(grafo, vector[1], vector[0]);
 	}
-
-	mostrar(grafo[4]);
-
-	return 0;
 }
